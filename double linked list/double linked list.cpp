d977e8393982eec60c5b6bec139cff95b9dffa2c
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-26 20:01:49 +0700

    menyisipkan node antara dua node dalam daftar

--- double linked list/double linked list.cpp
+++ double linked list/double linked list.cpp
@@ -10,46 +10,54 @@ public:
 	Node* prev;
 };
 
-class DoubleLinkedList {
+class DoubleLinkedlist {
 private:
 	Node* START = NULL;
-public:
+public:;
 
-	void addNode();
-	bool search(int rollNo, Node** previous, Node** current);
-	bool deleteNode(int rollNo);
-	bool ListEmpty();
-	void traverse();
-	void revtraverse();
-	void hapus();
-	void searchData();
-
+	  void addNode();
+	  bool search(int rollNo, Node** previous, Node** current);
+	  bool deleteNode(int rollNo);
+	  bool listEmpty();
+	  void traverse();
+	  void revtraverse();
+	  void hapus();
+	  void searchData();
 };
 
-void DoubleLinkedList::addNode() {
+void DoubleLinkedlist::addNode() {
 	int nim;
 	string nm;
-	cout << "\nEnter the roll number of the student:";
+	cout << "\nEnter the roll number of the student: ";
 	cin >> nim;
-	cout << "\nEnter the name of the srudent:";
+	cout << "\nEnter the name of the student: ";
 	cin >> nm;
 
-
 	Node* newNode = new Node(); //step 1
-	newNode->noMhs = nim; // step 2
+	newNode->noMhs = nim; //step 2
 	newNode->name = nm; //step 2
 
-	//*insert a node the begining of a doubly - linked list/
+	/*insert a node in the begining o a doubly - Linked List*/
 	if (START == NULL || nim <= START->noMhs) { //check if data null
 		if (START != NULL && nim == START->noMhs) {
-			cout << "\nDupLicate number not allowed" << endl;
+			cout << "\nDuplicate number not allowed" << endl;
 			return;
 		}
-		newNode->next = START; // step 3
+		newNode->next = START; //step 3
 		if (START != NULL)
 			START->prev = newNode; //step 4
-		newNode->prev = NULL; //step 5
+		newNode->prev = NULL; //step 5 
 		START = newNode; //step 6
 		return;
 	}
-}
+
+	/*inserting  a Node Between two Nodes in the List*/
+	Node* current = START; //1.a
+	Node* previous = NULL; //1.b
+	while (current->next != NULL && current->next->noMhs < nim) //step 1.c
+	{
+		previous = current; //1.d
+		current = current->next; //1.e
+	}
+
+	
