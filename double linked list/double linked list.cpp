4e4b8f1eb61d1643638b84084d49386426d10860
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-26 15:44:27 +0700

    membuat kelas double linked list

--- double linked list/double linked list.cpp
+++ double linked list/double linked list.cpp
@@ -9,3 +9,19 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START = NULL;
+public:
+
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool ListEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
+
+};
